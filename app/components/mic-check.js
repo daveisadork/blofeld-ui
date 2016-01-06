55b667f1f224074d2a9a677bdf34e310ec3bff94
Dave Hayes <dwhayes@gmail.com>
2016-01-06 05:41:59 -0600

    Initial working mic check implementation

--- app/components/mic-check.js
+++ app/components/mic-check.js
@@ -4,7 +4,11 @@ export default Ember.Component.extend({
   checkElement (element) {
     var that = this;
     var store = this.get('targetObject.store');
-    element.oncanplay = function () {
+    var handler = function (event) {
+      console.log(this.id + ': ' + event.type);
+      this.onsuspend = null;
+      this.onerror = null;
+      this.oncanplay = null;
       var keys = this.id.split('-');
       var method = keys[0];
       var name = keys[1];
@@ -24,19 +28,46 @@ export default Ember.Component.extend({
         }
       ).then(function (record) {
         record.set(method, supported);
-        record.save().then(function (record) {
+        record.save().then(function () {
           if (that.elements.length) {
             that.checkElement(that.elements.shift());
           }
         });
       });
     };
-    element.onerror = element.oncanplay;
-    element.load();
+    element.oncanplay = handler;
+    element.onerror = handler;
+    element.preload = 'auto';
+  },
+
+  checkAllElements() {
+    this.elements = this.$('#mic-check audio').toArray();
+    this.checkElement(this.elements.shift());
   },
 
   didInsertElement () {
-    this.elements = this.$('#mic-check audio').toArray();
-    this.checkElement(this.elements.shift());
+    var that = this;
+    var elements = this.$('#mic-check audio');
+    var store = this.get('targetObject.store');
+    store.findAll('format').then(function (records) {
+      if ((records.get('length') * 2) !== elements.length) {
+        that.checkAllElements();
+      }
+    });
+  },
+
+  actions: {
+    forceCheck () {
+      var store = this.get('targetObject.store');
+      var that = this;
+      store.findAll('format').then(
+        function (records) {
+          records.forEach(function (record) {
+            record.destroyRecord();
+          });
+          that.checkAllElements();
+        }
+      );
+    }
   }
 });
